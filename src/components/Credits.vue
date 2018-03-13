f218f7d7867f77cb8b15e4cb6f9838679862afd2
odayaka <odayakanaasa@gmail.com>
2018-03-14 00:44:32 +0700

    5

--- src/components/Credits.vue
+++ src/components/Credits.vue
@@ -1,10 +1,6 @@
 <template>
   <footer>
     <div class="me">
-			
-<?xml version="1.0" standalone="no"?>
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN"
- "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
 <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
  width="720.000000pt" height="721.000000pt" viewBox="0 0 720.000000 721.000000"
  preserveAspectRatio="xMidYMid meet">
